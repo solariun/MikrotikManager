5884a7ece2ed9fbddadff050a144198a09f0377c
Gustavo Campos <gustavocampos@Gustavos-Mac-mini.local>
2014-06-06 11:48:24 -0300

    Initial Commit

---
+++ MikrotikManager/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MikrotikManager
+//
+//  Created by Gustavo Campos on 6/6/14.
+//  Copyright (c) 2014 Gustavo Campos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
